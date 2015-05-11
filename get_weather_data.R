fd8538d5e981fe285213ebc02ad310773499bf5f
Tom Evans <thomas.jude.evans@gmail.com>
2015-05-11 23:20:41 +0200

    Getting data for all variables. Next to calculate means etc for these

--- get_weather_data.R
+++ get_weather_data.R
@@ -23,26 +23,154 @@ sk_location <- NULL
 sk_location$long <- 17.97
 sk_location$lat  <- 57.28
 
-i <- 1
+# i <- 1
 # For each flight do following
 # for(i in 1:length(trips$trip_id)){
   
-  
+# ?difftime  
+hours_6 <- as.difftime(6, units = "hours")
+
+
   # Get weather data at following time points:
     # for weather variables:
     #' Wind - S-N  (‘uwnd.sig995’ U-Wind Component [East/West] (Near Surface) m/s)
-   x <-    NCEP.interp("uwnd.sig995", "surface", sk_location$lat, sk_location$long,
-                  date_time[1], reanalysis2 = TRUE,
-                  interpolate.space = TRUE, interpolate.time = TRUE,
-                  keep.unpacking.info = TRUE, return.units = TRUE,
-                  interp = 'linear', p = 1, status.bar = FALSE)
-        
-    
+
+
+uwnd.10m.0h <-    NCEP.interp("uwnd.10m", "gaussian", sk_location$lat, sk_location$long,
+                              date_time, reanalysis2 = TRUE,
+                              interpolate.space = TRUE, interpolate.time = TRUE,
+                              keep.unpacking.info = TRUE, return.units = TRUE,
+                              interp = 'linear', p = 1, status.bar = FALSE)
+
+uwnd.10m.6h <-    NCEP.interp("uwnd.10m", "gaussian", sk_location$lat, sk_location$long,
+                              (date_time-hours_6), reanalysis2 = TRUE,
+                              interpolate.space = TRUE, interpolate.time = TRUE,
+                              keep.unpacking.info = TRUE, return.units = TRUE,
+                              interp = 'linear', p = 1, status.bar = FALSE)
+
+uwnd.10m.12h <-    NCEP.interp("uwnd.10m", "gaussian", sk_location$lat, sk_location$long,
+                              (date_time-(hours_6 * 2)), reanalysis2 = TRUE,
+                              interpolate.space = TRUE, interpolate.time = TRUE,
+                              keep.unpacking.info = TRUE, return.units = TRUE,
+                              interp = 'linear', p = 1, status.bar = FALSE)
+
+uwnd.10m.18h <-    NCEP.interp("uwnd.10m", "gaussian", sk_location$lat, sk_location$long,
+                               (date_time-(hours_6 * 2)), reanalysis2 = TRUE,
+                               interpolate.space = TRUE, interpolate.time = TRUE,
+                               keep.unpacking.info = TRUE, return.units = TRUE,
+                               interp = 'linear', p = 1, status.bar = FALSE)
+
+
+
     #' Wind - W-E  (‘vwnd.sig995’ V-Wind Component [North/South] (Near Surface) m/s)
-    #' Temp
+vwnd.10m.0h <-    NCEP.interp("vwnd.10m", "gaussian", sk_location$lat, sk_location$long,
+                              date_time, reanalysis2 = TRUE,
+                              interpolate.space = TRUE, interpolate.time = TRUE,
+                              keep.unpacking.info = TRUE, return.units = TRUE,
+                              interp = 'linear', p = 1, status.bar = FALSE)
+
+vwnd.10m.6h <-    NCEP.interp("vwnd.10m", "gaussian", sk_location$lat, sk_location$long,
+                              (date_time-hours_6), reanalysis2 = TRUE,
+                              interpolate.space = TRUE, interpolate.time = TRUE,
+                              keep.unpacking.info = TRUE, return.units = TRUE,
+                              interp = 'linear', p = 1, status.bar = FALSE)
+
+vwnd.10m.12h <-    NCEP.interp("vwnd.10m", "gaussian", sk_location$lat, sk_location$long,
+                               (date_time-(hours_6 * 2)), reanalysis2 = TRUE,
+                               interpolate.space = TRUE, interpolate.time = TRUE,
+                               keep.unpacking.info = TRUE, return.units = TRUE,
+                               interp = 'linear', p = 1, status.bar = FALSE)
+
+vwnd.10m.18h <-    NCEP.interp("vwnd.10m", "gaussian", sk_location$lat, sk_location$long,
+                               (date_time-(hours_6 * 2)), reanalysis2 = TRUE,
+                               interpolate.space = TRUE, interpolate.time = TRUE,
+                               keep.unpacking.info = TRUE, return.units = TRUE,
+                               interp = 'linear', p = 1, status.bar = FALSE)
+
+    
+    
+    #' Temp air.2m
+air.2m.0h <-    NCEP.interp("air.2m", "gaussian", sk_location$lat, sk_location$long,
+                              date_time, reanalysis2 = TRUE,
+                              interpolate.space = TRUE, interpolate.time = TRUE,
+                              keep.unpacking.info = TRUE, return.units = TRUE,
+                              interp = 'linear', p = 1, status.bar = FALSE)
+
+air.2m.6h <-    NCEP.interp("air.2m", "gaussian", sk_location$lat, sk_location$long,
+                              (date_time-hours_6), reanalysis2 = TRUE,
+                              interpolate.space = TRUE, interpolate.time = TRUE,
+                              keep.unpacking.info = TRUE, return.units = TRUE,
+                              interp = 'linear', p = 1, status.bar = FALSE)
+
+air.2m.12h <-    NCEP.interp("air.2m", "gaussian", sk_location$lat, sk_location$long,
+                               (date_time-(hours_6 * 2)), reanalysis2 = TRUE,
+                               interpolate.space = TRUE, interpolate.time = TRUE,
+                               keep.unpacking.info = TRUE, return.units = TRUE,
+                               interp = 'linear', p = 1, status.bar = FALSE)
+
+air.2m.18h <-    NCEP.interp("air.2m", "gaussian", sk_location$lat, sk_location$long,
+                               (date_time-(hours_6 * 2)), reanalysis2 = TRUE,
+                               interpolate.space = TRUE, interpolate.time = TRUE,
+                               keep.unpacking.info = TRUE, return.units = TRUE,
+                               interp = 'linear', p = 1, status.bar = FALSE)
+    
+    
+    
     #' Precipitation
-    #' Cloud cover
-  
+prate.sfc.0h <-    NCEP.interp("prate.sfc", "gaussian", sk_location$lat, sk_location$long,
+                            date_time, reanalysis2 = TRUE,
+                            interpolate.space = TRUE, interpolate.time = TRUE,
+                            keep.unpacking.info = TRUE, return.units = TRUE,
+                            interp = 'linear', p = 1, status.bar = FALSE)
+
+prate.sfc.6h <-    NCEP.interp("prate.sfc", "gaussian", sk_location$lat, sk_location$long,
+                            (date_time-hours_6), reanalysis2 = TRUE,
+                            interpolate.space = TRUE, interpolate.time = TRUE,
+                            keep.unpacking.info = TRUE, return.units = TRUE,
+                            interp = 'linear', p = 1, status.bar = FALSE)
+
+prate.sfc.12h <-    NCEP.interp("prate.sfc", "gaussian", sk_location$lat, sk_location$long,
+                             (date_time-(hours_6 * 2)), reanalysis2 = TRUE,
+                             interpolate.space = TRUE, interpolate.time = TRUE,
+                             keep.unpacking.info = TRUE, return.units = TRUE,
+                             interp = 'linear', p = 1, status.bar = FALSE)
+
+prate.sfc.18h <-    NCEP.interp("prate.sfc", "gaussian", sk_location$lat, sk_location$long,
+                             (date_time-(hours_6 * 2)), reanalysis2 = TRUE,
+                             interpolate.space = TRUE, interpolate.time = TRUE,
+                             keep.unpacking.info = TRUE, return.units = TRUE,
+                             interp = 'linear', p = 1, status.bar = FALSE)    
+    
+    
+    #' Cloud cover   tcdc.eatm
+tcdc.eatm.0h <-    NCEP.interp("tcdc.eatm", "gaussian", sk_location$lat, sk_location$long,
+                               date_time, reanalysis2 = TRUE,
+                               interpolate.space = TRUE, interpolate.time = TRUE,
+                               keep.unpacking.info = TRUE, return.units = TRUE,
+                               interp = 'linear', p = 1, status.bar = FALSE)
+
+tcdc.eatm.6h <-    NCEP.interp("tcdc.eatm", "gaussian", sk_location$lat, sk_location$long,
+                               (date_time-hours_6), reanalysis2 = TRUE,
+                               interpolate.space = TRUE, interpolate.time = TRUE,
+                               keep.unpacking.info = TRUE, return.units = TRUE,
+                               interp = 'linear', p = 1, status.bar = FALSE)
+
+tcdc.eatm.12h <-    NCEP.interp("tcdc.eatm", "gaussian", sk_location$lat, sk_location$long,
+                                (date_time-(hours_6 * 2)), reanalysis2 = TRUE,
+                                interpolate.space = TRUE, interpolate.time = TRUE,
+                                keep.unpacking.info = TRUE, return.units = TRUE,
+                                interp = 'linear', p = 1, status.bar = FALSE)
+
+tcdc.eatm.18h <-    NCEP.interp("tcdc.eatm", "gaussian", sk_location$lat, sk_location$long,
+                                (date_time-(hours_6 * 2)), reanalysis2 = TRUE,
+                                interpolate.space = TRUE, interpolate.time = TRUE,
+                                keep.unpacking.info = TRUE, return.units = TRUE,
+                                interp = 'linear', p = 1, status.bar = FALSE)    
+
+
+
+
+
     
   
   
