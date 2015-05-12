a134b8f7199f46aa9e6ba49ae07ea8e0be1f2d0c
Tom Evans <thomas.jude.evans@gmail.com>
2015-05-12 09:26:17 +0000

    Calculations on sunrise/ sunset times

--- time_of_day.R
+++ time_of_day.R
@@ -1,31 +1,37 @@
-
-tod <- seq(0,24,0.1)
-
-light_lev <- cos((pi*tod/12))
-all_day <-  cos((pi*tod/24))
-
-plot(light_lev~tod)
-points(all_day~tod, col = "red")
-
-?hour
-
-?switch
+# This script calculates time since sunrise, and calculates a variable based on that, plus specifies whether a trip is morning or afternoon (UTC).
 
 
-# Make an example date-time object
-x <- as.POSIXct(c("2014-05-02 12:45:00",
-                  "2014-05-02 10:45:00",
-                  "2014-05-02 19:45:00"), tz = "UTC")
+# Packages + data ----
+# install.packages("solaR")
+library("solaR")
+
+
+# Load trip data
+trips <- read.csv("foraging_trip_info_filtered_may2015_start_time_sunrise_only.csv", header = TRUE)
+
+
+# Check date_time format
+date_time_start <- as.POSIXct(trips$start_time, tz = "utc")
+date_time_sunrise <- as.POSIXct(trips$sunrise_date_time, tz = "utc")
+
+
+# Morning or evening? (according to solar time) --------
+
+# Convert start time to local solar time
+date_time_start_solar <- local2Solar(date_time_start, lon = 17.97)
+
+# See how this looks
+date_time_start_solar[1:5]
+date_time_start[1:5]
+
+
 
 # Extract the hour from the time
-x.hour <- as.numeric(format(x, "%H"))
-
+date_time_start.hour <- as.numeric(format(date_time_start_solar, "%H"))
+# hist(date_time_start.hour)
 # Label as AM or PM
 # Empty variable
 am.pm <- NULL
-
-# If before midday, label AM, else label PM
-if(x < 12) am.pm <- "AM" else am.pm <- "PM"
 
 # If you have a vector (list) of values, test this for all of these
 am.pm.fun <- function(x){
@@ -33,35 +39,61 @@ am.pm.fun <- function(x){
   return(am.pm)
 }
 
-am.pm.thing <- sapply(x.hour, am.pm.fun)
+am.pm <- sapply(date_time_start.hour, am.pm.fun)
+am.pm <- as.factor(am.pm)
+
+# Number of trips leaving in morning or afternoon
+summary(am.pm)
 
 
-
-
-
-# Time sinse sunrise
+# Time since sunrise   --------
 # (cos(π*Hours since sunrise/12))
 
-# Make example vector of sunrise times
-# Below are examples, not true sunrise times
-sunrise_times <- as.POSIXct(c("2014-05-02 05:45:00",
-                                   "2014-05-06 05:38:00",
-                                   "2014-08-10 06:04:00"), tz = "UTC")
 
-# Example trip departure times (NB sunrise time #1 above corresponds to trip departure time #1 below)
-trip_dept_time <-   as.POSIXct(c("2014-05-02 03:37:00",
-                                 "2014-05-06 11:54:00",
-                                 "2014-08-10 18:54:00"), tz = "UTC")
+# Calculate time since sunrise
+time_since_sunrise <- difftime(date_time_start,  date_time_sunrise, units = "hours")
+time_since_sunrise <- as.numeric(time_since_sunrise)
+str(time_since_sunrise)
+hist(time_since_sunrise, col = "grey")
 
 
 
-# Calculate time sinse sunrise
-x <- difftime(trip_dept_time,  sunrise_times, units = "hours")
-hours.since.sunrise <- as.numeric(x)
+# Double plot this data (i.e. plot it twice, as time before, and time after sunrise)
+# Time before
+range.fun <- function(x){
+  x <- x-24
+  if(x < -24){x <- (x+24)}
+  return(x)
+}
+
+# Time after
+range.fun.2 <- function(x){
+  if(x < 0){x <- (x+24)}
+  return(x)
+}
+
+x2 <- sapply(time_since_sunrise, range.fun)
+x3 <- sapply(time_since_sunrise,range.fun.2)
+hist(x2)
+hist(x3)
+# hist(time_since_sunrise)
+pdf("dept_time_sunrise.pdf")
+hist(rbind(x3, x2), xlim = c(-24,24), breaks = 48,
+     col = "grey", xlab = "Time since/before sunrise at departure time",
+     main = "Departure time relative to sunrise time (double plot)",
+     xaxt = "n")
+axis(1, at = seq(-24, 24, by = 4), las = 1)
+dev.off()
 
 
 # Calculate cos thing
-time.since.sunrise.cos <-  (cos(pi*hours.since.sunrise/12))
+time_since_sunrise.cos <-  (cos(pi*time_since_sunrise/12))
+
+hist(time_since_sunrise.cos)
+
+# Double plot this agains time since/ before sunrise
+plot(rbind(time_since_sunrise.cos, time_since_sunrise.cos)~rbind(x3, x2))
 
 
 
+# Output data to dataframe -----
