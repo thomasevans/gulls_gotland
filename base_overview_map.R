766249a83286118bcccb0b9af3cbb2eb689ff5d1
Tom Evans <thomas.jude.evans@gmail.com>
2015-07-29 20:35:20 +0000

    Some formatting etc.

--- base_overview_map.R
+++ base_overview_map.R
@@ -30,14 +30,14 @@ p3_sp <- SpatialPolygons(list(p3),proj4string=CRS("+init=epsg:4326"))
 
 # Large area  map
 # Plot map and output to file
-# png("test_map_1.png", width = 4*dpi, height = 5*dpi, res = dpi)
-win.metafile("test_map_1.wmf", width = 4, height = 5)
+# png("range_map.png", width = 5*dpi, height = 5*dpi, res = dpi)
+win.metafile("range_map.wmf", width = 4, height = 5)
 # ?pdf
 # Set map limits and format
 par(mfrow=c(1,1))
 par( mar = c(5, 4, 4, 2))
-c.xlim <- c(0,35)
-c.ylim = c(52,72)
+c.xlim <- c(0,30)
+c.ylim = c(53,71)
 
 # c.xlim <- c(17,20)
 # c.ylim = c(56.5,58)
@@ -57,16 +57,16 @@ plot(p3_sp , col = "dark grey", border = NA, add = T)
 
 map('worldHires', col="black", density = 10, bg = NA, add = TRUE)
 
-rect(17, 56.5, 20, 58, lwd = 1, lty = 1)
+rect(17.0, 56.9, 19, 57.6, lwd = 2, lty = 1)
 
 # Scale bar and axis
 x <- c.xlim[1] + (c.xlim[2] - c.xlim[1])/20
-y <- c.ylim[1] + (c.ylim[2] - c.ylim[1])/10
-map.scale(x, y, ratio = FALSE, relwidth = 0.25)
+y <- c.ylim[1] + 8.5*(c.ylim[2] - c.ylim[1])/10
+map.scale(x, y, ratio = FALSE, relwidth = 0.30, cex = 0.8)
 #   ?map.scale
 box()
-axis(side=(1),las=1, cex.axis = 0.8)
-axis(side=(2),las=1, cex.axis = 0.8)
+axis(side=(1),las=1, cex.axis = 0.6)
+axis(side=(2),las=1, cex.axis = 0.6)
 
 dev.off()
 
@@ -75,8 +75,8 @@ dev.off()
 
 # Large area  map
 # Plot map and output to file
-# png("test_map_2.png", width = 4*dpi, height = 5*dpi, res = dpi)
-win.metafile("test_map_2.wmf", width = 4, height = 5)
+# png("zoom_map.png", width = 5*dpi, height = 5*dpi, res = dpi)
+win.metafile("zoom_map.wmf", width = 5, height = 5)
 
 # ?png
 # Set map limits and format
@@ -84,8 +84,8 @@ par(mfrow=c(1,1))
 par( mar = c(5, 4, 4, 2))
 
 
-c.xlim <- c(17,19)
-c.ylim = c(56.5,58)
+c.xlim <- c(17.5, 18.8)
+c.ylim = c(56.9,57.6)
 
 # data(worldHiresMapEnv)
 
@@ -94,14 +94,26 @@ plot(gadm, xlim = c.xlim,
 
 
 # Add rectangle to indicate region in expansion map
-rect(17.9, 57.18, 18.4, 57.33, lwd = 1, lty = 1)
+rect(18.27, 57.21, 18.39, 57.247, col = "black", lwd = 1, lty = 1)
 
 # Scale bar and axis
 x <- c.xlim[1] + (c.xlim[2] - c.xlim[1])/20
-y <- c.ylim[1] + (c.ylim[2] - c.ylim[1])/10
-map.scale(x, y, ratio = FALSE, relwidth = 0.25)
+y <- c.ylim[1] + 9*(c.ylim[2] - c.ylim[1])/10
+map.scale(x, y, ratio = FALSE, relwidth = 0.2, cex = 0.8)
 box()
-axis(side=(1),las=1, cex.axis = 0.8)
-axis(side=(2),las=1, cex.axis = 0.8)
+axis(side=(1),las=1, cex.axis = 0.6)
+axis(side=(2),las=1, cex.axis = 0.6)
+
+text(x= c(17.95), y = c(57.25), labels = c("SK"), adj = NULL,
+     pos = NULL, offset = 0, vfont = NULL,
+     cex = 1.5, col = NULL, font = 3)
+
+text(x= c(18.4), y = c(57.45), labels = c("Gotland"), adj = NULL,
+     pos = NULL, offset = 0, vfont = NULL,
+     cex = 1.5, col = NULL, font = 3)
+
+text(x= c(17.97), y = c(57.285), labels = c("*"), adj = NULL,
+     pos = NULL, offset = 0, vfont = NULL,
+     cex = 2, col = NULL, font = 1)
 
 dev.off()
