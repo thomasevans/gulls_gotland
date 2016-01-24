a55283a38946a2b3b9d438e3fe525c64e37f2fd1
thomasevans <thomas.jude.evans@gmail.com>
2016-01-24 11:27:47 +0100

    Initial try at interpolating GPS tracks for later analysis

---
+++ interpolate_trips.R
@@ -0,0 +1,21 @@
+# Interpolate GPS data to regular time interval, either downsampling, or upsampling
+# when gaps in data exist
+
+
+# Load in the filtered trip data (from trip_departure_decision_filter.R)
+
+
+# Take a sub-set of these for initial testing
+
+
+# Get GPS point data for these trips
+
+
+# Make these into ltraj thing from adehabitat
+# Think can treat each trip as a 'burst'
+
+
+# Initially on one trip...
+
+# Process with redisltraj from adehabitatLT
+# Try 600 s initially - or maybe even 300 s???
