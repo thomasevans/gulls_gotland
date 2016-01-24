c6f17cb15b7f487812d5a7244aa39f074ddc75f7
thomasevans <thomas.jude.evans@gmail.com>
2016-01-24 21:33:11 +0100

    Script to annotate the interpolated trip points with additional variables

---
+++ label_interpolated_trip_points.R
@@ -0,0 +1,13 @@
+# Script to annotate the interpolated GPS data
+
+# Load in data (from R binnary) ------
+
+# Calculate distances between points -----
+# use deg.dist function
+# Set NA values for first location on each trip
+
+# Calculate distance from colony (use some centre location) ------
+
+# Label by weather on land/ sea --------
+# see previous on gotland thing and the Fågelsundet land/sea script
+
