341f5832a4cd5159a30f6c2653fa1295f42ba556
thomasevans <thomas.jude.evans@gmail.com>
2016-02-20 15:00:08 +0100

    Making BBMM kernel things and plotting ok map of these

--- kde_plots.R
+++ kde_plots.R
@@ -54,17 +54,95 @@ points(xy)
 # See: Kranstauber, B., Kays, R., LaPoint, S.D., Wikelski, M., Safi, K., 2012. A dynamic Brownian bridge movement model to estimate utilization distributions for heterogeneous animal movement. Journal of Animal Ecology.
 
 # Load package
-install.packages("BBMM")
+# install.packages("BBMM")
 library("BBMM")
+unique(points$ring_number)
+
+source("deg.dist.R")
+points$coldist <- deg.dist( 17.971676, 57.278817,
+                            points$longitude,
+                            points$latitutde)
+hist(points$coldist)
+
+points.sub <- (points$dist_max < 200) & (points$ring_number == 8114315)
+points.sub <- (points$dist_max < 200)
+points.sub <- (points$dist_max < 200) & (points$coldist > 3)
 
 
-?brownian.bridge
-BBMM <- brownian.bridge(x = points$longitude, y = points$latitutde, 
-                        time.lag = rep(5,nrow(points)), location.error = 20, 
-                        cell.size = 50)
-contours <- bbmm.contour(BBMM, levels=c(95, 99),
-                         locations=cbind.data.frame(points$longitude,
-                                                    points$latitutde), plot=TRUE)
+
+# range(points$dist_max)
+
+# ?brownian.bridge
+BBMM <- brownian.bridge(x = points$longitude[points.sub],
+                        y = points$latitutde[points.sub], 
+                        time.lag = rep(5,nrow(points[points.sub,])-1),
+                        location.error = 0.05, 
+                        cell.size = 0.05)
+bbmm.summary(BBMM)
+cont.levels <- c(seq(25, 75, by = 25), 90, 95, 99)
+contours <- bbmm.contour(BBMM, levels= cont.levels, plot = TRUE)
+points(x = points$longitude[points.sub], y = points$latitutde[points.sub])
+
+# contours <- bbmm.contour(BBMM, levels=c(95, 99),
+                         # locations=cbind.data.frame(points$longitude,
+                                                    # points$latitutde), plot=TRUE)
+
+library(maptools)
+library(raster)
+out <- data.frame(x=BBMM$x,y=BBMM$y,z=BBMM$probability)
+# Make sure the data is properly projected
+out.raster <- rasterFromXYZ(out,crs=CRS("+proj=utm +zone=12 +datum=WGS84"),digits=2) 
+raster.contour <- rasterToContour(out.raster,levels=contours$Z) 
+raster.contour <- spChFIDs(raster.contour,paste(cont.levels,"% Contour Line",sep=""))
+
+
+# Map the contours nicely -------
+# Maps package
+library(maps)
+
+# Load coastline data
+load("SWE_adm0.RData")
+
+
+pdf("map_thing.pdf", width = 5, height = 8)
 par(mfrow=c(1,1))
-plot(BBMM)
-summary(BBMM)
+par( mar = c(5, 4, 4, 2))
+
+plot(gadm, col= "grey40", bg = "white",
+     lty = 0,
+     xlim = c(17,19.2),
+     ylim = c(55.7,58.5))
+
+# Alpha channel ----
+addalpha <- function(colors, alpha=1.0) {
+  r <- col2rgb(colors, alpha=T)
+  # Apply alpha
+  r[4,] <- alpha*255
+  r <- r/255.0
+  return(rgb(r[1,], r[2,], r[3,], r[4,]))
+}
+
+
+points(x = points$longitude[points.sub], y = points$latitutde[points.sub],
+       cex = 0.2, col = addalpha("black", alpha = 0.2),
+       lwd = 0.4)
+
+plot(gadm, col = NA, bg = NA, lwd = 1, add = TRUE,
+     border = addalpha("blue", alpha = 0.5))
+
+# lwd.lines <- c(1:length(cont.levels))
+# lwd.lines <- max(lwd.lines)/c(1:length(cont.levels))
+# lwd.lines <- (lwd.lines/max(lwd.lines))*2.5
+plot(raster.contour, add = TRUE, col = addalpha("red", alpha = 0.6),
+     lwd = seq(1, 3, 2/length(cont.levels)))
+     # lty = c(1:length(cont.levels)))
+
+
+## Scale bar and axis
+box(col="black",lwd=2)
+axis(side=(1),las=1,col="black",col.axis="black")
+axis(side=(2),las=1,col="black",col.axis="black")
+
+map.scale(ratio = FALSE,
+          relwidth = 0.30, cex = 1)
+dev.off()
